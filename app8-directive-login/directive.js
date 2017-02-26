e4408e8c8791117c0ee80d540b9ae664f4c207d8
Jeff Chapman <jeffcchapman@gmail.com>
2017-02-25 21:56:35 -0700

    finished app8 and included jqlite focus on first input when page loads and when user submits

--- app8-directive-login/directive.js
+++ app8-directive-login/directive.js
@@ -1,6 +1,23 @@
 angular.module('app8')
   .directive('loginDirective', function(){
     return {
-      
+      restrict: "E",
+      templateUrl: 'loginDirectiveTempl.html',
+      scope: true,
+      controller: function($scope){
+        $scope.submit = function() {
+          alert('username is: '+ $scope.username + ' and password is: '+ $scope.password);
+          $scope.username = '';
+          $scope.password = '';
+          $scope.focusIt();
+        }
+
+      },
+      link: function(scope,elem,attrs) {
+        scope.focusIt = function() {
+          elem[0].firstChild[0].focus();
+        };
+        scope.focusIt();
+      }
     }
   })
