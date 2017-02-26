5cf0eaded1e18dbf2252668dea918b9507b6a6b9
Jeff Chapman <jeffcchapman@gmail.com>
2017-02-25 20:05:20 -0700

    changed template to templateUrl

--- app7-directive-template/directive.js
+++ app7-directive-template/directive.js
@@ -2,7 +2,6 @@ angular.module('app7')
   .directive('appDirective', function() {
     return {
       restrict: 'AE',
-      template:  '<header><a href="" style="text-decoration: none; margin: 50"><em>Page 1</em></a><a href="" style="text-decoration: none; margin: 50"><strong>Page 2</strong></a><a href="" style="text-decoration: none; margin: 50">Page 3</a></header>',
-
+      templateUrl: 'appDirectiveTempl.html'
     }
   })
